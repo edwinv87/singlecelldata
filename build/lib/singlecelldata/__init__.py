a0fb32c9b8aa0fceb465ba2814c9b5d632747be9
Edwin Vans <vans.edw@gmail.com>
2020-07-01 22:47:11 +1200

    Update __init__.py

--- build/lib/singlecelldata/__init__.py
+++ build/lib/singlecelldata/__init__.py
@@ -1,1 +1,13 @@
-from .singlecelldata import SingleCell
+"""
+SingleCellData 
+==============
+
+A Python package that contains a class for managing single-cell RNA-seq datasets.
+See https://edwinv87.github.io/singlecelldata/ for more information and documentation. 
+
+"""
+
+from .singlecell import SingleCell
+
+
+__all__ = ['SingleCell']
