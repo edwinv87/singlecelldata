0efb4b497220d4f5484d9afc4560611db465196b
Edwin Vans <vans.edw@gmail.com>
2021-10-11 13:34:00 +1200

    Update read_data.py

--- singlecelldata/read_data.py
+++ singlecelldata/read_data.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import numpy as np
 
-import gzip
+# import gzip
 import os.path
-import scipy.sparse
+# import scipy.sparse
 from scipy.sparse import csr_matrix
+
+
 
 def ReadCSV(path, dset_name):
 
@@ -12,19 +14,31 @@ def ReadCSV(path, dset_name):
     celldata_path = path + dset_name + '/' + dset_name + "_celldata.csv"
     genedata_path = path + dset_name + '/' + dset_name + "_genedata.csv"
 
-    data = pd.read_csv(data_path, index_col=0)
-    celldata = pd.read_csv(celldata_path, index_col=0)
-    genedata = pd.read_csv(genedata_path, index_col = 0)
+    if os.path.isfile(data_path):
+        data = pd.read_csv(data_path, index_col=0)
+    else: 
+        msg = "File <" + data_path + "> does not exist"
+        raise ValueError(msg)
+
+    if os.path.isfile(celldata_path):
+        celldata = pd.read_csv(celldata_path, index_col=0)
+    else:
+        celldata = None
+
+    if os.path.isfile(genedata_path):
+        genedata = pd.read_csv(genedata_path, index_col = 0)
+    else:
+        genedata = None 
+    
 
     return data, genedata, celldata
 
 
 
 
+def Read10X(path, dset_name):
 
-def Read10X(path):
-
-    with open(path + '/matrix.mtx', 'r') as f:
+    with open(path + dset_name + '/matrix.mtx', 'r') as f:
         while True:
             header = f.readline()
             if not header.startswith('%'):
@@ -38,17 +52,17 @@ def Read10X(path):
             data.append(float(fields[2]))
             i.append(int(fields[1])-1)
             j.append(int(fields[0])-1)
-        X = csr_matrix((data, (i, j)), shape=(n_cells, n_genes))
+        X = csr_matrix((data, (i, j)), shape=(n_cells, n_genes)).toarray()
 
     genes = []
-    with open(path + '/genes.tsv', 'r') as f:
+    with open(path + dset_name + '/genes.tsv', 'r') as f:
         for line in f:
             fields = line.rstrip().split()
             genes.append(fields[1])
     assert(len(genes) == n_genes)
 
     barcodes = []
-    with open(path + '/barcodes.tsv', 'r') as f:
+    with open(path + dset_name + '/barcodes.tsv', 'r') as f:
         for line in f:
             fields = line.rstrip().split()
             barcodes.append(fields[1])
